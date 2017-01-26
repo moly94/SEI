4f2424cbb383c40fe56ee0e0422613aeee0fe646
Student <Student@VM-macOS-1012.local>
2017-01-26 15:33:48 +0100

    Finished

--- Assignment03/ViewController.swift
+++ Assignment03/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        lblpin.text="PIN: "
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        lblpin.text="PIN: "
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,60 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btn0(_ sender: AnyObject) {
+        lblpin.text?.append("0")
+    }
+    
+    
+    @IBAction func btn1(_ sender: AnyObject) {
+        lblpin.text?.append("1")
+    }
+    
+    
+    @IBAction func btn2(_ sender: AnyObject) {
+        lblpin.text?.append("2")
+    }
+   
+    @IBAction func btn3(_ sender: AnyObject) {
+        lblpin.text?.append("3")
+    }
+    
+    
+    @IBAction func btn4(_ sender: AnyObject) {
+        lblpin.text?.append("4")
+    }
+    
+    
+    
+    @IBAction func btn5(_ sender: AnyObject) {
+        lblpin.text?.append("5")
+    }
+    
+    
+    @IBAction func btn6(_ sender: AnyObject) {
+        lblpin.text?.append("6")
+    }
+    
+    
+    
+    @IBAction func btn7(_ sender: AnyObject) {
+        lblpin.text?.append("7")
+    }
+    
+    
+    @IBAction func btn8(_ sender: AnyObject) {
+        lblpin.text?.append("8")
+    }
+    
+    
+    @IBAction func btn9(_ sender: AnyObject) {
+        lblpin.text?.append("9")
+        
+    }
+    
+    
+    @IBOutlet weak var btn6: UIButton!
+    
+    @IBOutlet weak var lblpin: UILabel!
 }
 
