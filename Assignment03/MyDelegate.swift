f5900e41def94d6395a797a4e65434d5486e24d5
Student <Student@Students-MacBook-Pro.local>
2017-01-29 19:47:00 +0100

    Added some core dafat features, app fails at build time :(

---
+++ Assignment03/MyDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  myDelegate.swift
+//  Assignment03
+//
+//  Created by Student on 29/01/2017.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class MyDelegate: UIResponder, UIApplicationDelegate {
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "Model")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+    })
+        return container
+    }()
+    
+    
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
--- Assignment03/NewItemViewController.swift
+++ Assignment03/NewItemViewController.swift
@@ -6,11 +6,21 @@
 //  Copyright © 2017 Student. All rights reserved.
 //
 
+
 import Foundation
+import CoreData
 
 import UIKit
 
 class NewItemViewController: UIViewController {
+    
+    
+    lazy var managedContext: NSManagedObjectContext? = {
+        guard let appDelegate = UIApplication.shared.delegate as? MyDelegate else {
+            return nil
+        }
+        return appDelegate.persistentContainer.viewContext
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +31,18 @@ class NewItemViewController: UIViewController {
     
     @IBAction func btnDone(_ sender: AnyObject) {
         
+        let data = txtInput.text
+        let bucketTask = NSEntityDescription.insertNewObject(forEntityName: "BucketTask", into: self.managedContext!) as! BucketTask; bucketTask.content=data
+        
+        saveStuff()
         
         self.performSegue(withIdentifier: "goBack", sender: self)
+    }
+    
+    func saveStuff() {
+        guard let myDelegate = UIApplication.shared.delegate as? MyDelegate else {
+            return
+        }
+        myDelegate.saveContext()
     }
 }
